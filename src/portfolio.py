b37844e8c9fe6cdaeaa75eddbf52b605fab6b078
Andrew Nicholls <andrewsnicholls@gmail.com>
2018-08-30 11:57:52 -0700

    Worked on Portfolio Class Methods

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,12 +1,12 @@
 from dateutil.relativedelta import relativedelta
 
 class Loan:
-    def __init__(self, loan_id, loan_size, dollars_invested):
+    def __init__(self, loan_id, loan_size):
             self.id = loan_id
             self.size = loan_size
-            self.investment_amount = dollars_invested
-            self.principal_balance = dollars_invested
+            self.principal_balance = 0
             self.status = 'Current'
+            self.months_since_last_payment = 0
     
     def default(self):
         self.status = 'Default'
@@ -16,13 +16,14 @@ class Loan:
         self.principal_balance = (self.investment_amount / self.size) * overall_principal
 
 class Portfolio:
-    def __init__(self, starting_balance, investment_per_loan, start_date):
+    def __init__(self, starting_balance, investment_per_loan, start_date, min_roi=5.0):
         self.active_loans = []
         self.defaulted_loans = []
         self.cash_balance = starting_balance
         self.invested_principal_balance = 0
         self.investment_per_loan = investment_per_loan
         self.date = start_date
+        self.min_roi = min_roi
 
     def update_invested_principal_balance(self):
         self.invested_principal_balance = sum([loan.principal_balance for loan in self.active_loans])
@@ -36,8 +37,40 @@ class Portfolio:
     def increment_date_by_one_month(self):
         self.date += relativedelta(months=1)
 
-def get_payments_for_date(payments_df, date):
-    '''
-    date parameter needs to be of type datetime.date
-    '''
-    return payments_df[(payments_df['RECEIVED_D'].dt.year == date.year) & (payments_df['RECEIVED_D'].dt.month == date.month)]
+    def purchase_loans(self, loans):
+        for loan in loans:
+            self.active_loans.append(loan)
+
+    def convert_df_rows_to_loans(self, df):
+        loans = []
+        rows = df.to_dict(orient='records')
+        for row in rows:
+            loans.append(Loan(row['id'], row['loan_amnt']))
+        return loans
+
+    def get_loans_available_for_given_date(self, loans_df):
+        '''
+        date parameter needs to be of type datetime.date
+        '''
+        return loans_df[(loans_df['issue_d'].dt.year == date.year) & (loans_df['issue_d'].dt.month == date.month)]
+
+    def get_top_n_loans_to_buy(self, loans, n):
+        loans.sort_values(by='predicted_roi', ascending=False, inplace=True)
+        return loans.head(n)
+
+    def get_loans_over_required_roi_threshold(self, df, min_roi):
+        return df[df['predicted_roi'] >= min_roi]
+
+    def get_payments_for_date(self, payments_df, date):
+        '''
+        date parameter needs to be of type datetime.date
+        '''
+        return payments_df[(payments_df['RECEIVED_D'].dt.year == date.year) & (payments_df['RECEIVED_D'].dt.month == date.month)]
+
+
+def get_top_n_loans_to_buy(loans, n):
+    loans.sort_values(by='predicted_roi', ascending=False, inplace=True)
+    return loans.head(n)
+
+def add():
+    pass
