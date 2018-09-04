d8f452393c0dd11c8f9a0e7841d46dd14e59f453
Andrew Nicholls <andrewsnicholls@gmail.com>
2018-09-04 09:40:03 -0700

    Added aws-scripts.py file
    File for storing functions used to process data on AWS EC2 instance.

--- src/aws-scripts.py
+++ src/aws-scripts.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import pandas as pd
 import boto3
 import multiprocessing as mp
 import pickle
@@ -14,3 +15,10 @@ def load_data_from_s3(filename, format='csv'):
         df = pd.read_pickle(f, compression='bz2')
     return df
     
+def load_raw_data_froms3(filename, format='csv'):
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket='loan-analysis-data', Key=filename)
+    data = obj['Body'].read()
+    f = BytesIO(data)
+    if format=='csv':
+        df = pd.read_csv(f, low_memory=False)
