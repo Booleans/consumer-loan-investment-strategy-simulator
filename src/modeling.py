34dc74d786e4aa2818a3cea47d0cae47efff35e0
Andrew Nicholls <andrewsnicholls@gmail.com>
2018-09-05 12:16:20 -0700

    Updated Modeling.py
    The function to create a dataframe for  simulation purposes has been changed to set issue date as the index instead of loan id.

--- src/modeling.py
+++ src/modeling.py
@@ -11,8 +11,8 @@ def get_training_and_testing_data(df, split_date='2015-09-01'):
     Loans before the cutoff date will be used for training the model, the others will be used for
     simulating and evaluating the model's performance.
     '''
-    training_loans = df[df['issue_d'].isin(pd.date_range('2005-01-01', split_date)) == True]
-    testing_loans = df[df['issue_d'].isin(pd.date_range('2005-01-01', split_date)) == False]
+    training_loans = df[df['issue_d'].isin(pd.date_range('2010-01-01', split_date)) == True]
+    testing_loans = df[df['issue_d'].isin(pd.date_range('2010-01-01', split_date)) == False]
     return training_loans, testing_loans
 
 def train_model(model, X_train, y_train):
@@ -25,4 +25,4 @@ def get_predictions(fit_model, X):
 def create_dataframe_for_simulation(loan_df, predictions):
     simulation_df = loan_df.copy(deep=True)
     simulation_df['predicted_roi'] = predictions
-    return simulation_df[['issue_d', 'loan_amnt', 'predicted_roi']]
+    return simulation_df[['issue_d', 'loan_amnt', 'predicted_roi']].reset_index(level=0).set_index('issue_d')
